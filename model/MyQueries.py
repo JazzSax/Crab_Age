60d8ca4566f66c7e85a066f7f90c5839ede85874
JazzSax <jhnbrnrdshgn@gmail.com>
2024-05-14 10:46:23 +0800

    initial commit

---
+++ model/MyQueries.py
@@ -0,0 +1,12 @@
+import sys
+from os import path
+script_dir = path.dirname(__file__)
+sys.path.append(script_dir)
+from MyConnection import MyConnection
+
+
+class MyQueries(): 
+        
+        def __init__(self) -> None:
+                pass
+                
