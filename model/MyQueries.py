13311aa72b04c5670160f7fa0b18a5e45c66cf3e
JazzSax <jhnbrnrdshgn@gmail.com>
2024-05-16 13:38:09 +0800

    added queries

--- model/MyQueries.py
+++ model/MyQueries.py
@@ -1,12 +1,25 @@
-import sys
-from os import path
-script_dir = path.dirname(__file__)
-sys.path.append(script_dir)
 from MyConnection import MyConnection
 
 
 class MyQueries(): 
         
         def __init__(self) -> None:
-                pass
-                
+                self.conn = MyConnection("localhost","root","","crab")
+                self.mydb = self.conn.connect()
+        
+        def addRecord(self, sex, length, diameter, height, weight, shucked, viscera, shell,age ):
+                mycursor = self.mydb.cursor()
+                sql = "INSERT INTO info (sex,length,diameter,height,weight,shucked,viscera,shell, age) VALUES ('"+str(sex)+"','"+str(length)+"','"+str(diameter)+"','"+str(height)+"','"+str(weight)+"','"+str(shucked)+"','"+str(viscera)+"','"+str(shell)+"','"+str(age)+"')"
+                mycursor.execute(sql)
+                self.mydb.commit()
+                result=mycursor.rowcount, "Record Added!"  
+                return result
+        def showAll(self):
+                myCursor = self.mydb.cursor(dictionary=True)
+                myCursor.execute("Select * From info")
+                result = myCursor.fetchall()
+                return result
+
+        
+
+       
