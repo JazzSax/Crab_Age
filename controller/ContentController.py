34d3caaf8b7e0dd4b563c516bbbc77ceb2c6e2e4
JazzSax <jhnbrnrdshgn@gmail.com>
2024-05-30 09:22:27 +0800

    final na

--- controller/ContentController.py
+++ controller/ContentController.py
@@ -40,25 +40,30 @@ if(clear):
     from Functions import input
     input()
 if (getPredict):
+
     from Functions import output, predict
-    age = predict(getSex, getLength, getDiameter, getHeight, getWeight, getShucked, getViscera, getShell)
-    if(age <= 3):
-        result = f"Crabs are not ready for harvesting. You might want to wait {6-age} for optimal harvest"
-    if(age>3 and age<=5):
-        result = f"Crabs are too early to be harvest. You might want to wait {6-age}"
-    if(age>5 and age<=7):
+   
+    age = float(predict(getSex, getLength, getDiameter, getHeight, getWeight, getShucked, getViscera, getShell))
+   
+    if(age <= 10):
+        result = f"Crabs are not ready for harvesting. You might want to wait {round(15-age)} months for optimal harvest"
+    elif(age>10 and age<=13):
+        result = f"Crabs are too early to be harvest. You might want to wait {round(15-age)} months before harvesting"
+    elif(age>13 and age<=15):
         result = "Crabs are ready for harvest. Harvest now"
-    if(age>7 and age <= 9):
+    elif(age>15 and age <= 17):
         result = f"Crabs are past the optimal age for harvesting. But still can be harvested."
-    if(age>9):
+    elif(age>17):
         result = f"Crabs are past harvesting. It is recommended to mate with other crab to create more crabs!"
-    
+    else:
+        result = f"Not Proper Age of Crab"
+   
     sys.path.append(script_dir)
     from model.MyQueries import MyQueries
     query = MyQueries()
     results = query.addRecord(getSex, getLength, getDiameter, getHeight, getWeight, getShucked, getViscera, getShell,age)
 
-    output(results,getSex, getLength, getDiameter, getHeight, getWeight, getShucked, getViscera, getShell,age)
+    output(result,results,getSex, getLength, getDiameter, getHeight, getWeight, getShucked, getViscera, getShell,age)
     
     # input()
    
